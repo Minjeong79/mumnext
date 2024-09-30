af9413ce7499b039e8637ec7c255db40f5cc4af1
jeungsugi <yoouug72@gmail.com>
2024-09-30 18:22:47 +0900

    매니페스트 작업중

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next';
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Next.js PWA',
+    short_name: 'NextPWA',
+    description: 'A Progressive Web App built with Next.js',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: "/public/icons/1.png",
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: "/public/icons/3.png",
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
+
