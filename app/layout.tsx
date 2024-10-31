f7034fe89e0f4e4d183d1a306ad16707eecd5bc6
jeungminjeung <ru--io@naver.com>
2024-10-31 22:39:21 +0900

    배포 수정3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import ClientLayout from "./clayout/ClientLayout";
 import Head from "next/head";
 export const metadata = {
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "강아지 기록 다이어리",
+  description: "강아지 기록 다이어리 와 커뮤니티",
 };
 
 export default function RootLayout({
@@ -12,17 +12,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
- 
   return (
     <html lang="kr" className={GeistSans.className}>
       <body className="">
-      <Head>
-        <link rel="manifest" href="/manifest.ts" />
-      </Head>
+        <Head>
+          <link rel="manifest" href="/manifest.ts" />
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="connect-src 'self' https://trtwwyqzkqlqebdiiujp.supabase.co https://apis.data.go.kr https://sockjs-us3.pusher.com;"
+          />
+        </Head>
         <ClientLayout>{children}</ClientLayout>
-       
       </body>
-    
     </html>
   );
 }
